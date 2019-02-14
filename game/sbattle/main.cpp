b322a0a3f8ea639c9e2108feba583bf6f765f3a6
Сергей Черевко <s.cherevko@emzior.ru>
2019-02-14 18:39:02 +0300

    start tcpserver

--- game/sbattle/main.cpp
+++ game/sbattle/main.cpp
@@ -1,16 +1,46 @@
 #include "mainwindow.h"
+#include <stdlib.h>
+#include <QtGui>
+#include <QtCore>
+#include <QtNetwork>
 #include <QApplication>
-#include "battleserver.h"
-#include <QUdpSocket>
 #include <QTcpServer>
 #include <QTcpSocket>
+#include <QMessageBox>
+
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
 
-    QTcpServer server();
+    QTcpServer *tcpServer = new QTcpServer();
+    qDebug() << tcpServer->serverPort();
+
+    QString ipAddress;
+    QList<QHostAddress> ipAddressesList = QNetworkInterface::allAddresses();
+
+    // используем первый не локальный адрес IPv4
+    for (int i = 0; i < ipAddressesList.size(); ++i) {
+        if (ipAddressesList.at(i) != QHostAddress::LocalHost &&
+            ipAddressesList.at(i).toIPv4Address()) {
+            ipAddress = ipAddressesList.at(i).toString();
+            if (!tcpServer->listen(ipAddressesList.at(i), 35135)) {
+                QMessageBox::critical(NULL, QObject::tr("Fortune Server"),
+                                      QObject::tr("Unable to start the server: %1.")
+                                      .arg(tcpServer->errorString()));
+                qDebug() << "Can't start listen on port " << tcpServer->serverPort();
+                return 1;
+            }
+            else {
+                qDebug() << "Start listen on port " << tcpServer->serverPort();
+//                QMessageBox::information(NULL, QObject::tr("SBattle Server"),
+//                                      QObject::tr("Start the server: %1.")
+//                                      .arg(tcpServer->errorString()));
+            }
+            break;
+        }
+    }
 
     w.show();
 
